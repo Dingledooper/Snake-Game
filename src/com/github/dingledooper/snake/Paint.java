0029022d9a404ce1b503fd764a601b4e24deb4cc
Lyndon Ho <44751422+Dingledooper@users.noreply.github.com>
2019-09-08 15:43:44 -0700

    Update Paint.java

--- src/com/github/dingledooper/snake/Paint.java
+++ src/com/github/dingledooper/snake/Paint.java
@@ -36,9 +36,6 @@ public class Paint {
 	}
 	
 	public void render() {
-		g2d.setColor(Color.LIGHT_GRAY);
-		g2d.fillRect(0, 0, Game.WIDTH, Game.BORDERY);
-		
 		g2d.setColor(new Color(0, 100, 0));
 		g2d.setStroke(new BasicStroke(4));
 		g2d.drawRect(2, Game.BORDERY, Game.WIDTH - 2, Game.HEIGHT - Game.BORDERY - 2);
@@ -50,6 +47,9 @@ public class Paint {
 		
 		g2d.setColor(Color.RED);
 		g2d.fillRect(cherry.getX() * Cherry.SCALE, cherry.getY() * Cherry.SCALE, Cherry.SIZE, Cherry.SIZE);
+		
+		g2d.setColor(Color.LIGHT_GRAY);
+		g2d.fillRect(0, 0, Game.WIDTH, Game.BORDERY);
 		
 		g2d.setColor(Color.DARK_GRAY);
 		g2d.setFont(new Font("Arial", Font.PLAIN, 24));
@@ -63,6 +63,9 @@ public class Paint {
 		g2d.drawString("GAME OVER!", (Game.WIDTH - g2d.getFontMetrics().stringWidth("GAME OVER!")) / 2, 300);
 		g2d.drawString("Your score:", (Game.WIDTH - g2d.getFontMetrics().stringWidth("Your score:")) / 2, 360);
 		g2d.drawString(scoreString, (Game.WIDTH - g2d.getFontMetrics().stringWidth(scoreString)) / 2, 420);
+		
+		g2d.setFont(new Font("Monospaced", Font.PLAIN, 30));
+		g2d.drawString("Press ENTER to try again", (Game.WIDTH - g2d.getFontMetrics().stringWidth("Press ENTER to try again")) / 2, 460);
 	}
 
 }
